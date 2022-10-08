7f70bfd52d91e4a029f178aec3a5c6750376dd95
Zibi95 <sylwester.sob95@gmail.com>
2022-10-08 13:56:08 +0200

    Add eventListener on buttons Add screenDisplay function Add clear function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+const operation = document.querySelector(".operation");
+const buttons = document.querySelectorAll(".buttons");
+
+buttons.forEach((el) =>
+  el.addEventListener("click", function (e) {
+    if (e.target.innerText === "CE") {
+      clear();
+      return;
+    } else if (e.target.innerText === "=") {
+      operate(operation.innerText);
+      return;
+    }
+    screenUpload(e.target.innerText);
+  })
+);
+
+function screenUpload(content) {
+  operation.innerText += content;
+}
+
+function clear() {
+  operation.innerText = "";
+  operation.previousSibling.innerText = "";
+}
